83637eecfda5f75d5fd8a3b042ca93924da5f18b
kakum3 <104210758+kakum3@users.noreply.github.com>
2022-10-07 10:01:02 +0200

    first commit

---
+++ src/component/Todo.js
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+
+const Todo = ({ item,onUpdate,onDelete }) => {
+  const [isEdit, setIsEdit] = useState(false);
+
+  const FormEdit = () => {
+    const[newValue,setNewValue]=useState(item.title);
+
+
+    const handleSubmit = (e) => {
+      e.preventDefault();
+    }
+    const handleChange=(e)=>{
+      const value=e.target.value;
+      setNewValue(value);
+    }
+    const handleClickUpdateTodo=()=>{
+      onUpdate(item.id,newValue);
+      setIsEdit(false);
+
+    }
+
+    return (
+      <form className="todoUpdateForm" onSubmit={handleSubmit}>
+        <input type="text" className="todoInput" onChange={handleChange} value={newValue} />
+        <button className="button"onClick={handleClickUpdateTodo}>Update</button>
+      </form>
+    );
+  };
+  const TodoElement = () => {
+    return (
+      <div className="todoInfo">
+      <span className="todoTitle">  {item.title}</span>
+        <button className="button" onClick={() => setIsEdit(true)}>Editar</button>
+        <button className="buttonDelete" onClick={(e)=>onDelete(item.id)}>Eliminar</button>
+      </div>
+    );
+  };
+  return <div className="todo">{isEdit ? <FormEdit /> : <TodoElement />}</div>;
+};
+
+export default Todo;
